492563fd2dc6ac2087974f4655428d29da26f417
Misha1337 <mishanyakolpakov@gmail.com>
2017-08-02 22:54:12 +0300

    primitive singleton added

--- Pattern/src/com/mykhailo/kolpakov/creational/Singleton.java
+++ Pattern/src/com/mykhailo/kolpakov/creational/Singleton.java
@@ -2,6 +2,26 @@ package com.mykhailo.kolpakov.creational;
 
 public class Singleton {
 	public static void main(String[] args) {
+			int MAX_SIZE = 1000;
+			Single[] singles = new Single[MAX_SIZE];
+			
+			for(int i = 0; i < MAX_SIZE; i++) {
+				singles[i] = Single.getInstance();
+			}
+			System.out.println(Single.counter);
+	}
+}
 
+class Single {
+	private Single() { counter++;};
+
+	public static int counter = 0;
+	private static Single instance;
+
+	
+	public static Single getInstance() {
+		if (instance == null)
+			instance = new Single();
+		return instance;
 	}
 }
