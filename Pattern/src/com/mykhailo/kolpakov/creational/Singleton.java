ff3cc292abc50858c870628da34e004f68741148
Misha1337 <mishanyakolpakov@gmail.com>
2017-08-03 12:40:38 +0300

    Singleton with multithreading added

--- Pattern/src/com/mykhailo/kolpakov/creational/Singleton.java
+++ Pattern/src/com/mykhailo/kolpakov/creational/Singleton.java
@@ -1,27 +1,60 @@
 package com.mykhailo.kolpakov.creational;
 
 public class Singleton {
-	public static void main(String[] args) {
-			int MAX_SIZE = 1000;
-			Single[] singles = new Single[MAX_SIZE];
-			
-			for(int i = 0; i < MAX_SIZE; i++) {
-				singles[i] = Single.getInstance();
-			}
-			System.out.println(Single.counter);
+	public static void main(String[] args) throws InterruptedException {
+		int MAX_SIZE = 1000;
+		Thread[] threads = new Thread[MAX_SIZE];
+		for (int i = 0; i < MAX_SIZE; i++) {
+			threads[i] = new Thread(new R());
+			threads[i].start();
+		}
+		for (int i = 0; i < MAX_SIZE; i++) {
+			threads[i].join();
+		}
+		System.out.println(Single.counter);
 	}
 }
 
+class R implements Runnable {
+
+	@Override
+	public void run() {
+		Single.getInstance();
+	}
+
+}
+
 class Single {
-	private Single() { counter++;};
+	private Single() {
+		counter++;
+	};
 
 	public static int counter = 0;
-	private static Single instance;
+	private static volatile Single instance = null;
 
-	
+	/*
+	 * public static Single getInstance() { return instance; }
+	 */
+	/*
+	 * public static Single getInstance() { if (instance == null) instance = new
+	 * Single(); return instance; }
+	 */
+	/*
+	 * public static synchronized Single getInstance() { if (instance == null)
+	 * instance = new Single();
+	 * 
+	 * return instance; }
+	 */
+
 	public static Single getInstance() {
-		if (instance == null)
-			instance = new Single();
+		if (instance == null) {
+			synchronized (Single.class) {
+				if (instance == null) {
+					instance = new Single();
+				}
+			}
+		}
 		return instance;
 	}
+
 }
