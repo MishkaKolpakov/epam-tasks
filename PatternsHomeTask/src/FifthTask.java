0ba1c7811fc6b95f0014e4916d9fa6fff59653e1
Misha1337 <mishanyakolpakov@gmail.com>
2017-08-10 22:25:24 +0300

    PatternsHomeTask 1, 5 tasks added

---
+++ PatternsHomeTask/src/FifthTask.java
@@ -0,0 +1,86 @@
+import java.util.Arrays;
+
+public class FifthTask {
+	public static void main(String[] args) {
+		Salad salad = new Salad.SaladBuilder(Vegatables.CABBAGE, Vegatables.TOMATO)
+				.addSauce(Sauce.MUSHROOM)
+				.addCheese(Cheese.CHEDDAR)
+				.build();
+		System.out.println(salad.getSalad());
+	}
+}
+
+class Salad {
+	private Cheese cheese = Cheese.MOZZARELLA;
+	private Vegatables[] vegatables = { Vegatables.TOMATO, Vegatables.ONIONS };
+	private Sauce sauce = Sauce.MUSHROOM;
+
+	public Cheese getCheese() {
+		return cheese;
+	}
+
+	public Vegatables[] getVegatables() {
+		return vegatables;
+	}
+
+	public Sauce getSauce() {
+		return sauce;
+	}
+	
+	public String getSalad() {
+		return this.toString();
+	}
+
+	@Override
+	public String toString() {
+		return "Salad consists of: "+ cheese + ", " + Arrays.toString(vegatables) + ", " + sauce;
+	}
+
+	private Salad(SaladBuilder saladBuilder) {
+		this.cheese = saladBuilder.cheese;
+		this.vegatables = saladBuilder.vegatables;
+		this.sauce = saladBuilder.sauce;
+	}
+
+	static class SaladBuilder {
+		private Cheese cheese = Cheese.MOZZARELLA;
+		private Vegatables[] vegatables = { Vegatables.TOMATO, Vegatables.ONIONS };
+		private Sauce sauce = Sauce.MUSHROOM;
+		
+		public SaladBuilder(Vegatables... vegatables) {
+			this.vegatables = vegatables;
+		}
+		
+		public SaladBuilder addCheese(Cheese cheese) {
+			this.cheese = cheese;
+			return this;
+		}
+		
+		public SaladBuilder addVegatables(Vegatables... vegatables) {
+			this.vegatables = vegatables;
+			return this;
+		}
+		
+		public SaladBuilder addSauce(Sauce sauce) {
+			this.sauce = sauce;
+			return this;
+		}
+		
+		public Salad build() {
+			return new Salad(this);
+		}
+	}
+
+}
+
+enum Cheese {
+	CHEDDAR, MOZZARELLA, MASCARPONE;
+}
+
+enum Vegatables {
+	ONIONS, TOMATO, CABBAGE;
+}
+
+enum Sauce {
+	SALSA, MUSHROOM, WINE;
+}
---
+++ PatternsHomeTask/src/FirstTask.java
@@ -0,0 +1,52 @@
+
+public class FirstTask {
+	public static void main(String[] args) {
+		Human human = new Human();
+		human.setState(new StayOnBeach());
+		human.doAction();
+		human.setState(new SeeAnimal());
+		human.doAction();
+	}
+}
+
+interface State {
+	void doAction(Human human);
+}
+
+class StayOnBeach implements State {
+	@Override
+	public void doAction(Human human) {
+		System.out.println("Staying on beach");
+	}
+
+}
+
+class StayOnMushroomField implements State {
+	@Override
+	public void doAction(Human human) {
+		System.out.println("Staying on mushroom field");
+	}
+
+}
+
+class SeeAnimal implements State {
+	@Override
+	public void doAction(Human human) {
+		System.out.println("Seeing animal");
+	}
+
+}
+
+class Human {
+
+	private State state;
+
+	public void setState(State state) {
+		this.state = state;
+	}
+
+	public void doAction() {
+		state.doAction(this);
+	}
+
+}
